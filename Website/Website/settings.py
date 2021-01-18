8b33ae8ad3448b3ff3324f73b42fa9b4f044fca0
Abdullah <mabdulla@bates.edu>
2021-01-17 23:22:22 -0500

    Added .gitignore and removed SECRET_KEY

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from .secret import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_)s5ez=(8a=ggo*mmqztnk=l&x3qlkym9#vz&9ozq06fby4mz@'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +82,7 @@ DATABASES = {
         'USER': 'mabdulla',
         'PASSWORD': 'testpassword',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
